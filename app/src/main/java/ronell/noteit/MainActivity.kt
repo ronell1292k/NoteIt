2c0104f775283ef3a128b4bc0a447c29a0408807
Ronell Salunke <ronell1292k@gmail.com>
2021-05-16 14:16:06 +0530

    MainActivity: Implement navigation
    Signed-off-by: Ronell Salunke <ronell1292k@gmail.com>

--- app/src/main/java/ronell/noteit/MainActivity.kt
+++ app/src/main/java/ronell/noteit/MainActivity.kt
@@ -2,17 +2,11 @@ package ronell.noteit
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import ronell.noteit.ui.home.HomeFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, HomeFragment.newInstance())
-                .commitNow()
-        }
     }
 }
