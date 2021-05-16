2a229a63d636c60b3ce31c7ea4743b358318b73f
Ronell Salunke <ronell1292k@gmail.com>
2021-05-16 13:10:51 +0530

    home: Refactor module main as home
    Signed-off-by: Ronell Salunke <ronell1292k@gmail.com>

--- app/src/main/java/ronell/noteit/ui/home/HomeFragment.kt
+++ app/src/main/java/ronell/noteit/ui/home/HomeFragment.kt
@@ -1,4 +1,4 @@
-package ronell.noteit.ui.main
+package ronell.noteit.ui.home
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -8,24 +8,24 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import ronell.noteit.R
 
-class MainFragment : Fragment() {
+class HomeFragment : Fragment() {
 
     companion object {
-        fun newInstance() = MainFragment()
+        fun newInstance() = HomeFragment()
     }
 
-    private lateinit var viewModel: MainViewModel
+    private lateinit var viewModel: HomeViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
+        return inflater.inflate(R.layout.home_fragment, container, false)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        viewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
         // TODO: Use the ViewModel
     }
 
--- app/src/main/java/ronell/noteit/ui/home/HomeViewModel.kt
+++ app/src/main/java/ronell/noteit/ui/home/HomeViewModel.kt
@@ -1,7 +1,7 @@
-package ronell.noteit.ui.main
+package ronell.noteit.ui.home
 
 import androidx.lifecycle.ViewModel
 
-class MainViewModel : ViewModel() {
+class HomeViewModel : ViewModel() {
     // TODO: Implement the ViewModel
 }
