c18e58d0ef00a68e43c30c6a6b680775fec16d35
Ronell Salunke <ronell1292k@gmail.com>
2021-05-16 14:00:20 +0530

    home_fragment: Implement home fragment, import RecyclerView and MDC dependencies. Add new note FAB icon
    Signed-off-by: Ronell Salunke <ronell1292k@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,6 +46,8 @@ dependencies {
     implementation("androidx.room:room-common:2.3.0")
     implementation("androidx.room:room-runtime:2.3.0")
     implementation("androidx.navigation:navigation-fragment:2.3.5")
+    implementation("com.google.android.material:material:1.3.0")
+    implementation("androidx.recyclerview:recyclerview:1.1.0")
     testImplementation("junit:junit:4.+")
     androidTestImplementation("androidx.test.ext:junit:1.1.2")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.3.0")
