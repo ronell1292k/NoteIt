e9aba15af25b94cf5676868f9f06b29bfb74a74b
Ronell Salunke <ronell1292k@gmail.com>
2021-05-19 18:03:59 +0530

    build: Use Navigation KTX dependency
    Signed-off-by: Ronell Salunke <ronell1292k@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,7 +49,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.3.1")
     implementation("androidx.room:room-common:2.3.0")
     implementation("androidx.room:room-runtime:2.3.0")
-    implementation("androidx.navigation:navigation-fragment:2.3.5")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.3.5")
     implementation("com.google.android.material:material:1.3.0")
     implementation("androidx.recyclerview:recyclerview:1.1.0")
     testImplementation("junit:junit:4.+")
