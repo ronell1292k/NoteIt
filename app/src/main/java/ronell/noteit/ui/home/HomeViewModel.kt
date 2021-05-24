bfa6149126e6f559ff56dd5a828eadf82e2174ea
Ronell Salunke <ronell1292k@gmail.com>
2021-05-24 16:46:49 +0530

    HomeViewModel: Add note-fetching logic
    Signed-off-by: Ronell Salunke <ronell1292k@gmail.com>

--- app/src/main/java/ronell/noteit/ui/home/HomeViewModel.kt
+++ app/src/main/java/ronell/noteit/ui/home/HomeViewModel.kt
@@ -1,7 +1,17 @@
 package ronell.noteit.ui.home
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import ronell.noteit.database.Note
+import ronell.noteit.repository.NoteRepository
 
-class HomeViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class HomeViewModel(repository: NoteRepository) : ViewModel() {
+
+    private val _allNotes = repository.getNotes
+    private var allNotes: LiveData<List<Note>>
+        get() = _allNotes
+
+    init {
+        allNotes = _allNotes
+    }
 }
