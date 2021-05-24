e15fd0354163a452eadb54631ce33ed1b7d63f77
Ronell Salunke <ronell1292k@gmail.com>
2021-05-24 17:01:21 +0530

    HomeFragment: Cleanup, use 'by viewModels' as ViewModelProvider is deprecated
    Signed-off-by: Ronell Salunke <ronell1292k@gmail.com>

--- app/src/main/java/ronell/noteit/ui/home/HomeFragment.kt
+++ app/src/main/java/ronell/noteit/ui/home/HomeFragment.kt
@@ -6,18 +6,14 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import androidx.fragment.app.viewModels
 import androidx.navigation.Navigation
 import ronell.noteit.R
 import ronell.noteit.databinding.HomeFragmentBinding
 
 class HomeFragment : Fragment(R.layout.home_fragment) {
 
-    companion object {
-        fun newInstance() = HomeFragment()
-    }
-
-    private lateinit var viewModel: HomeViewModel
+    private val viewModel: HomeViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -35,9 +31,4 @@ class HomeFragment : Fragment(R.layout.home_fragment) {
         return binding.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
-        // TODO: Use the ViewModel
-    }
 }
