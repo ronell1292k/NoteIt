56330cb9cb822103e796ae05730eeb43f1b7cc85
Ronell Salunke <ronell1292k@gmail.com>
2021-06-02 11:40:42 +0530

    Implement save button
    Signed-off-by: Ronell Salunke <ronell1292k@gmail.com>

--- app/src/main/java/ronell/noteit/ui/edit/EditFragment.kt
+++ app/src/main/java/ronell/noteit/ui/edit/EditFragment.kt
@@ -1,17 +1,20 @@
 package ronell.noteit.ui.edit
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import ronell.noteit.R
+import ronell.noteit.database.Note
 import ronell.noteit.databinding.EditFragmentBinding
 
 class EditFragment : Fragment() {
     private val viewModel: EditViewModel by viewModels()
+    private var _binding: EditFragmentBinding? = null
+    private val binding get() = _binding!!
+    private var note: Note? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,6 +26,35 @@ class EditFragment : Fragment() {
             container,
             false
         )
+
+        setHasOptionsMenu(true)
+
         return binding.root
     }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.edit_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.save_button -> {
+                if (note != null) {
+                    saveNote()
+                }
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun saveNote() {
+        val title = binding.editTitle.text.toString()
+        val content = binding.editNote.text.toString()
+
+        val note = Note(title, content)
+        viewModel.insertNote(note)
+        Toast.makeText(context, "Note saved!", Toast.LENGTH_SHORT).show()
+    }
+
 }
