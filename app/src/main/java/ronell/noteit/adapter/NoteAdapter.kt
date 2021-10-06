037c150b0356307b216646897104bd002438b5da
Ronell Salunke <ronell1292k@gmail.com>
2021-10-06 16:54:33 +0530

    Use RecyclerView's ListAdapter and DiffUtil for grid
    Signed-off-by: Ronell Salunke <ronell1292k@gmail.com>

--- app/src/main/java/ronell/noteit/adapter/NoteAdapter.kt
+++ app/src/main/java/ronell/noteit/adapter/NoteAdapter.kt
@@ -1,24 +1,14 @@
 package ronell.noteit.adapter
 
-import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import ronell.noteit.database.Note
 import ronell.noteit.databinding.NoteCardBinding
 
-class NoteAdapter : RecyclerView.Adapter<NoteAdapter.NoteViewHolder>() {
-    private var notesList = emptyList<Note>()
-
-    class NoteViewHolder(private val binding: NoteCardBinding) :
-        ViewHolder(binding.root) {
-        fun bind(note: Note, context: Context) {
-            binding.textViewTitle.text = note.noteTitle
-            binding.textViewNote.text = note.noteBody
-            binding.executePendingBindings()
-        }
-    }
+class NoteAdapter : ListAdapter<Note, NoteAdapter.NoteViewHolder>(DiffCallback()) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
         val binding = NoteCardBinding.inflate(LayoutInflater.from(parent.context), parent, false)
@@ -26,10 +16,26 @@ class NoteAdapter : RecyclerView.Adapter<NoteAdapter.NoteViewHolder>() {
     }
 
     override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
-        val note = notesList[position]
-        holder.bind(note, holder.itemView.context)
+        val item = getItem(position)
+        holder.bind(item)
     }
 
-    override fun getItemCount(): Int = notesList.size
+    class NoteViewHolder(private val binding: NoteCardBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(note: Note) {
+            binding.textViewTitle.text = note.noteTitle
+            binding.textViewNote.text = note.noteBody
+        }
+    }
+
+    class DiffCallback : DiffUtil.ItemCallback<Note>() {
+        override fun areItemsTheSame(oldItem: Note, newItem: Note): Boolean {
+            return oldItem.noteId == newItem.noteId
+        }
+
+        override fun areContentsTheSame(oldItem: Note, newItem: Note): Boolean {
+            return oldItem == newItem
+        }
+    }
 
 }
